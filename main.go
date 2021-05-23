a60cb57555ce1cefafdb1ac6e5aa48abf45a6666
farrej10 <jamesfarrell@live.ie>
2021-05-23 21:54:33 +0100

    init

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	PageTitle string
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	data := Movie{
+		PageTitle: "My TODO list",
+	}
+
+	t, _ := template.ParseFiles("templates/layout.html")
+	t.Execute(w, data)
+}
+
+func main() {
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", indexHandler)
+	http.ListenAndServe(":8085", myRouter)
+}
