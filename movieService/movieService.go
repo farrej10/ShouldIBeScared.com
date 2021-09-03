715973f26fb612c68d91154e6740123328820016
James <farrej10@tcd.ie>
2021-09-03 18:18:03 +0100

    added proper movie struct

--- movieService/movieService.go
+++ movieService/movieService.go
@@ -20,14 +20,6 @@ const (
 	port = ":50051"
 )
 
-type Movie struct {
-	Title       string
-	Description string
-	Timestamp   string
-	ImageURL    string
-	Id          string
-}
-
 type MoviemangerServer struct {
 	pb.UnimplementedMoviemangerServer
 }
@@ -53,6 +45,54 @@ type Recommendations struct {
 	} `json:"results"`
 	TotalPages   int `json:"total_pages"`
 	TotalResults int `json:"total_results"`
+}
+
+type Movie struct {
+	Adult               bool   `json:"adult"`
+	BackdropPath        string `json:"backdrop_path"`
+	BelongsToCollection struct {
+		ID           int    `json:"id"`
+		Name         string `json:"name"`
+		PosterPath   string `json:"poster_path"`
+		BackdropPath string `json:"backdrop_path"`
+	} `json:"belongs_to_collection"`
+	Budget int `json:"budget"`
+	Genres []struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+	} `json:"genres"`
+	Homepage            string  `json:"homepage"`
+	ID                  int     `json:"id"`
+	ImdbID              string  `json:"imdb_id"`
+	OriginalLanguage    string  `json:"original_language"`
+	OriginalTitle       string  `json:"original_title"`
+	Overview            string  `json:"overview"`
+	Popularity          float64 `json:"popularity"`
+	PosterPath          string  `json:"poster_path"`
+	ProductionCompanies []struct {
+		ID            int    `json:"id"`
+		LogoPath      string `json:"logo_path"`
+		Name          string `json:"name"`
+		OriginCountry string `json:"origin_country"`
+	} `json:"production_companies"`
+	ProductionCountries []struct {
+		Iso31661 string `json:"iso_3166_1"`
+		Name     string `json:"name"`
+	} `json:"production_countries"`
+	ReleaseDate     string `json:"release_date"`
+	Revenue         int    `json:"revenue"`
+	Runtime         int    `json:"runtime"`
+	SpokenLanguages []struct {
+		EnglishName string `json:"english_name"`
+		Iso6391     string `json:"iso_639_1"`
+		Name        string `json:"name"`
+	} `json:"spoken_languages"`
+	Status      string  `json:"status"`
+	Tagline     string  `json:"tagline"`
+	Title       string  `json:"title"`
+	Video       bool    `json:"video"`
+	VoteAverage float64 `json:"vote_average"`
+	VoteCount   int     `json:"vote_count"`
 }
 
 func goDotEnvVariable(key string) string {
@@ -91,17 +131,17 @@ func (s *MoviemangerServer) GetMovie(ctx context.Context, in *pb.Params) (*pb.Mo
 		log.Fatalln(err)
 	}
 
-	data := Movie{
+	movie := &pb.Movie{
 		Title:       reading["original_title"].(string),
 		Description: reading["overview"].(string),
 		Timestamp:   "updated now!",
-		ImageURL:    "https://image.tmdb.org/t/p/w1280" + reading["backdrop_path"].(string),
+		Url:         "https://image.tmdb.org/t/p/w1280" + reading["backdrop_path"].(string),
 		Id:          strconv.Itoa(int(reading["id"].(float64))),
 	}
 
 	log.Println("Sending Data Now")
 
-	return &pb.Movie{Title: data.Title, Description: data.Description, Timestamp: data.Timestamp, Url: data.ImageURL}, nil
+	return movie, nil
 }
 
 func (s *MoviemangerServer) GetMovies(ctx context.Context, in *pb.Params) (*pb.Movies, error) {
